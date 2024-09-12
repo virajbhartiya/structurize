521792c35a03383f84dd6e71c1bb505842cc91f8
Viraj Bhartiya <vlbhartiya@gmail.com>
2024-09-13 01:44:22 +0530

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './styles/index.css' 
+import './styles/index.css'
 import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
+      <Analytics />
       <App />
     </ThemeProvider>
   </React.StrictMode>
